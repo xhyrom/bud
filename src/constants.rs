aaea5a245fd88b07ec22c392f38cc7f0f07f345d
xHyroM <generalkubo@gmail.com>
2023-05-28 16:49:24 +0200

    fix: support for linux & macos

--- src/constants.rs
+++ src/constants.rs
@@ -2,10 +2,15 @@ use lazy_static::lazy_static;
 
 #[cfg(target_os = "windows")]
 lazy_static! {
-  pub static ref FOLDER: String = format!("{}\\Bud", std::env::var("APPDATA").unwrap());
+    pub static ref FOLDER: String = format!("{}\\Bud", std::env::var("APPDATA").unwrap());
 }
 
-#[cfg(target_os = "unix")]
+#[cfg(target_os = "linux")]
 lazy_static! {
-  pub static ref FOLDER: String = format!("{}/.bud", std::env::var("HOME").unwrap());
+    pub static ref FOLDER: String = format!("{}/.bud", std::env::var("HOME").unwrap());
+}
+
+#[cfg(target_os = "macos")]
+lazy_static! {
+    pub static ref FOLDER: String = format!("{}/.bud", std::env::var("HOME").unwrap());
 }
