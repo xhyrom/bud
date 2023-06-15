96220db3d54472f5c0fc8004d28d4d07649bae13
xHyroM <generalkubo@gmail.com>
2023-06-15 17:39:47 +0200

    refactor!: remove config command
    for now

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -2,7 +2,6 @@ use clap::{arg, Command};
 
 use crate::file_configuration;
 
-mod config_command;
 mod new_command;
 mod version_command;
 
@@ -10,7 +9,6 @@ fn command() -> Command {
     Command::new("bud")
         .version("0.1.0")
         .author("xHyroM")
-        .subcommand(config_command::new())
         .subcommand(new_command::new())
         .subcommand(version_command::new())
         .args(&[arg!(--"global-config-path" [path] "Path to the global config file")])
@@ -26,7 +24,6 @@ pub fn handle() {
     file_configuration::initialize(&matches);
 
     match matches.subcommand() {
-        Some(("config", matches)) => config_command::handle(matches),
         Some(("new", matches)) => new_command::handle(matches),
         Some(("version", _)) => version_command::handle(),
         _ => unreachable!(),
